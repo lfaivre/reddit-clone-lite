86bbd79721ba3804d61a0509c29799dc5bce4919
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-11-20 18:49:51 -0700

    feat: add me query
    fix: fix urql caching issue

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,20 +1,39 @@
-import { Box, Flex, Link } from '@chakra-ui/core';
+import { Box, Button, Flex, Link } from '@chakra-ui/core';
 import NextLink from 'next/link';
 import React from 'react';
+import { useMeQuery } from '../generated/graphql';
 
 type NavBarProps = {};
 
 export const NavBar: React.FC<NavBarProps> = () => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={`auto`}>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link mr={2}>Login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link>Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={`auto`}>{body}</Box>
     </Flex>
   );
 };
